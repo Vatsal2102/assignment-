02cefd6995c79fc0b23baaf554254b50025a525b
Htet_Aung <htet4.aung@live.uwe.ac.uk>
2025-02-04 11:52:15 +0000

    Add configuration file with API keys and database connection settings

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+namespace dsa_twin_cities;
+@date_default_timezone_set("GMT");
+
+#API keys
+define ('OpenWeatherAPI', '');
+define ('GoogleAPI', '');
+define ('FlickrAPI', '');
+
+#Database connection
+define ('DBMS', [
+    'Host' => '',
+    'DB' => '',
+    'User' => '',
+    'Password' => ''
+    
+]);
+
+#

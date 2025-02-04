746cb6e0175adb3cce22550695a7d2b5194c0495
Hsu Myat San <hsu2.san@live.uwe.ac.uk>
2025-02-04 12:30:22 +0000

    llll

--- config.php
+++ config.php
@@ -1,19 +1,21 @@
 <?php
+
 namespace dsa_twin_cities;
+
 @date_default_timezone_set("GMT");
 
 #API keys
-define ('OpenWeatherAPI', '');
-define ('GoogleAPI', '');
-define ('FlickrAPI', '');
+define('OpenWeatherAPI', '');
+define('GoogleAPI', '');
+define('FlickrAPI', '');
 
 #Database connection
-define ('DBMS', [
+define('DBMS', [
     'Host' => '',
     'DB' => '',
     'User' => '',
     'Password' => ''
-    
+
 ]);
 
-#
+#TEST

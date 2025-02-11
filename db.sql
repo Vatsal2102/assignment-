caaf4cf772c07583a86733883d60f5f8e97f1da9
Htet_Aung <htet4.aung@live.uwe.ac.uk>
2025-02-11 12:57:35 +0000

    Update db.sql: add country column to city table and establish foreign key relationships for cityID in related tables

--- db.sql
+++ db.sql
@@ -1,7 +1,8 @@
--- Create a table for the city
+-- Create a table for city
 CREATE TABLE city (
     cityID INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
+    country VARCHAR(255) NOT NULL,
     latitude DECIMAL(9,6) NOT NULL,
     longitude DECIMAL(9,6) NOT NULL,
     timezone VARCHAR(255) NOT NULL,
@@ -10,20 +11,14 @@ CREATE TABLE city (
     countrycode CHAR(2) NOT NULL,
     currencyID INT NOT NULL,
     ethnicityID INT NOT NULL,
-    newsID INT NOT NULL,
-    photoID INT NOT NULL,
-    commentID INT NOT NULL,
-    placeID INT NOT NULL,
     PRIMARY KEY (cityID),
     FOREIGN KEY (currencyID) REFERENCES currency(currencyID),
-    FOREIGN KEY (ethnicityID) REFERENCES ethnicity(ethnicityID),
-    FOREIGN KEY (newsID) REFERENCES news(newsID),
-    FOREIGN KEY (photoID) REFERENCES photo(photoID),
-    FOREIGN KEY (commentID) REFERENCES comment(commentID),
-    FOREIGN KEY (placeID) REFERENCES place(placeID)
+    FOREIGN KEY (ethnicityID) REFERENCES ethnicity(ethnicityID)
 );
 
--- Create a table for the currency
+show tables;
+
+-- Create a table for currency
 CREATE TABLE currency (
     currencyID INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
@@ -44,7 +39,9 @@ CREATE TABLE news (
     title VARCHAR(255) NOT NULL,
     content TEXT NOT NULL,
     date DATE NOT NULL,
-    PRIMARY KEY (newsID)
+    cityID INT NOT NULL,
+    PRIMARY KEY (newsID),
+    FOREIGN KEY (cityID) REFERENCES city(cityID)
 );
 
 -- Create a table for photo
@@ -53,7 +50,9 @@ CREATE TABLE photo (
     title VARCHAR(255) NOT NULL,
     image BLOB NOT NULL,
     date DATE,
-    PRIMARY KEY (photoID)
+    cityID INT NOT NULL,
+    PRIMARY KEY (photoID),
+    FOREIGN KEY (cityID) REFERENCES city(cityID)
 );
 
 -- Create a table for comment
@@ -61,7 +60,9 @@ CREATE TABLE comment (
     commentID INT NOT NULL AUTO_INCREMENT,
     content TEXT NOT NULL,
     date DATE NOT NULL,
-    PRIMARY KEY (commentID)
+    cityID INT NOT NULL,
+    PRIMARY KEY (commentID),
+    FOREIGN KEY (cityID) REFERENCES city(cityID)
 );
 
 -- Create a table for place
@@ -71,7 +72,11 @@ CREATE TABLE place (
     description TEXT NOT NULL,
     latitude DECIMAL(9,6) NOT NULL,
     longitude DECIMAL(9,6) NOT NULL,
-    PRIMARY KEY (placeID)
+    cityID INT NOT NULL,
+    categoryID INT NOT NULL,
+    PRIMARY KEY (placeID),
+    FOREIGN KEY (cityID) REFERENCES city(cityID),
+    FOREIGN KEY (categoryID) REFERENCES category(categoryID)
 );
 
 -- Create a table for category
@@ -80,15 +85,14 @@ CREATE TABLE category (
     name VARCHAR(255) NOT NULL,
     PRIMARY KEY (categoryID)
 );
-
 -----------------------------------------------------------------
 
 -- Insert data into city table
 INSERT INTO city 
-    (name, latitude, longitude, timezone, language, population, countrycode, currencyID, ethnicityID, newsID, photoID, commentID, placeID)
+    (name, country, latitude, longitude, timezone, language, population, countrycode, currencyID, ethnicityID)
         VALUES 
-        ('Birmingham', 52.4862, -1.8904, 'GMT', 'English', 1141816, 'GB', 1, 1, 1, 1, 1, 1),
-        ('Frankfurt', 50.1109, 8.6821, 'CET', 'German', 753056, 'DE', 2, 2, 2, 2, 2, 2);
+        ('Birminham', 'United Kingdom', 52.4862, -1.8904, 'GMT', 'English', 1085810, 'GB', 1, 1),
+        ('Frankfurt', 'Germany', 50.1109, 8.6821, 'CET', 'German', 753056, 'DE', 2, 2);
 
 -- Insert data into currency table
 INSERT INTO currency 
